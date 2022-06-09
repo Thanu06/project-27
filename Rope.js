1ef0669c0adff3d8b4e5359b0e68b60e1567591e
Thanu06 <70212045+Thanu06@users.noreply.github.com>
2022-06-09 17:13:32 +0530

    Update Rope.js

--- Rope.js
+++ Rope.js
@@ -1,31 +1,24 @@
-class Rope{
-    constructor(body1, body2,xOffset)
-    {
-        var options = {
-            bodyA: body1,
-            bodyB: body2,
-            pointB:{x:xOffset,y:0}
-        }
-        this.rope = Constraint.create(options);
-        this.xOffset = xOffset;
-        World.add(world, this.rope);
-    }
-
-    display()
-    {
-        var pointA = this.rope.bodyA.position;
-        var pointB = this.rope.bodyB.position;
-        
-        var Anchor1X=pointA.x
-        var Anchor1Y=pointA.y
-
-        var Anchor2X=pointB.x+this.xoffset
-        var Anchor2Y=pointB.y
-
-        line(Anchor1X,Anchor1Y,Anchor2X,Anchor2Y);
-
-        stroke("black");
-        strokeWeight(3);
-    }
+class Rope {
     
+constructor(bodyA,bodyB,xOffset){
+    var options ={
+        bodyA:bodyA,
+        bodyB:bodyB,
+        pointB:{x:xOffset,y:0}      
+    }
+
+    this.rope=Constraint.create(options);
+    this.xOffset = xOffset;
+    World.add(world,this.rope);
+}
+
+display(){
+    var pointA = this.rope.bodyA.position;     
+    var pointB = this.rope.bodyB.position;     
+
+    stroke("black");
+    strokeWeight(3);
+
+    line(pointA.x,pointA.y,pointB.x + this.xOffset,pointB.y)
+}
 }
